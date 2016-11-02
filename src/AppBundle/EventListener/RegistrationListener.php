4967b82ce525578d3c82324d177edfe797b1f5e9
Tano Abeleyra <tanoabeleyra@gmail.com>
2016-11-02 10:04:46 -0300

    Registro: Usuario deshabilitado por defecto

---
+++ src/AppBundle/EventListener/RegistrationListener.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace AppBundle\EventListener;
+
+
+use FOS\UserBundle\Event\UserEvent;
+use FOS\UserBundle\FOSUserEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class RegistrationListener implements EventSubscriberInterface
+{
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            FOSUserEvents::REGISTRATION_INITIALIZE => 'onRegistrationInitialize',
+        ];
+    }
+
+    public function onRegistrationInitialize(UserEvent $event)
+    {
+        $event->getUser()->setEnabled(false);
+    }
+
+}
