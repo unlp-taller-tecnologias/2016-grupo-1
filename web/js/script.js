6106c9937a2f2d1653714c5f330f0e9050efe207
Simón Gotta <simon.gotta@gmail.com>
2016-11-06 16:44:18 -0300

    Integración de template + adaptación de ABM localidad + ABM partido a dicho template

--- web/js/script.js
+++ web/js/script.js
@@ -3,3 +3,34 @@ $('#fos_user_registration_form_profesion').change(function () {
     var medicoSelected = ($(this).val() == 1);
     $('#registration-medico-fields').toggle(medicoSelected);
 });
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+(function ($) {
+    // Handling the modal confirmation message.
+    $(document).on('submit', 'form[data-confirmation]', function (event) {
+        var $form = $(this),
+            $confirm = $('#confirmationModal');
+
+        if ($confirm.data('result') !== 'yes') {
+            //cancel submit event
+            event.preventDefault();
+
+            $confirm
+                .off('click', '#btnYes')
+                .on('click', '#btnYes', function () {
+                    $confirm.data('result', 'yes');
+                    $form.find('input[type="submit"]').attr('disabled', 'disabled');
+                    $form.submit();
+                })
+                .modal('show');
+        }
+    });
+})(window.jQuery);
